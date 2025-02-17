5b2e1c90ce1f6bc4af2d087f6b86f0023ce0ec70
Caio Torres Seares <caiot.seares@gmail.com>
2025-02-17 10:04:32 -0300

    http get call

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -10,7 +11,7 @@ import { Course } from '../model/course';
 export class CoursesComponent implements OnInit {
 
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
