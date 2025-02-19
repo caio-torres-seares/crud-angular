8dd08d798b6e53065f62bf324ecda47b458c1104
Caio Torres Seares <caiot.seares@gmail.com>
2025-02-19 08:24:08 -0300

    feat: add new route for form and create course-form

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -15,11 +16,13 @@ export class CoursesComponent implements OnInit {
 
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.courses$ = this.coursesService.list()
     .pipe(
@@ -34,8 +37,10 @@ export class CoursesComponent implements OnInit {
       this.dialog.open(ErrorDialogComponent, {
         data: errorMsg
       });
+  }
 
-
+  onAdd(){
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
   ngOnInit(): void {
